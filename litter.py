514cf5c2219fa2eb1ab4afc5b182bf96bf21cb7a
James Rowe <jnrowe@gmail.com>
2010-03-23 06:23:21 +0000

    Added license and metadata.

--- litter.py
+++ litter.py
@@ -1,4 +1,40 @@
 #! /usr/bin/python -tt
+# vim: set sw=4 sts=4 et tw=80 fileencoding=utf-8:
+#
+"""litter - Nasty little twitter client"""
+# Copyright (C) 2010  James Rowe <jnrowe@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+__version__ = "0.1.0"
+__date__ = "2010-03-23"
+__author__ = "James Rowe <jnrowe@gmail.com>"
+__copyright__ = "Copyright (C) 2010 James Rowe <jnrowe@gmail.com>"
+__license__ = "GNU General Public License Version 3"
+__credits__ = ""
+__history__ = "See git repository"
+
+from email.utils import parseaddr
+
+__doc__ += """.
+
+A simple little twitter client that display notifications for new tweets and
+nothing more.
+
+.. moduleauthor:: `%s <mailto:%s>`__
+""" % parseaddr(__author__)
 
 import atexit
 import json
