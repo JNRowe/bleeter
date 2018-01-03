9493303021ba265861b2429baf5da5d0ba5df05e
James Rowe <jnrowe@gmail.com>
2018-01-03 07:54:52 +0000

    Move setup() metadata to setup.cfg
    This is mostly about using a generic template, so that I'm not trying to
    sync thirty different versions of the same code.  We're not there yet,
    but we're on the way.

--- setup.py
+++ setup.py
@@ -1,54 +1,110 @@
 #! /usr/bin/env python3
-"""setup - distutils info for bleeter"""
-# Copyright (C) 2010-2012  James Rowe <jnrowe@gmail.com>
+"""setup.py - Setuptools tasks and config for bleeter."""
+# Copyright © 2010-2012  James Rowe <jnrowe@gmail.com>
 #
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# This file is part of bleeter.
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# bleeter is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
 #
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# bleeter is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
 #
+# You should have received a copy of the GNU General Public License along with
+# bleeter.  If not, see <http://www.gnu.org/licenses/>.
 
+from configparser import ConfigParser
 from importlib.util import module_from_spec, spec_from_file_location
 
 from setuptools import setup
+from setuptools.command.test import test
+
+
+class PytestTest(test):
+    def finalize_options(self):
+        test.finalize_options(self)
+        self.test_args = ['tests/', ]
+        self.test_suite = True
+
+    def run_tests(self):
+        from sys import exit
+        from pytest import main
+        exit(main(self.test_args))
 
 
 def import_file(package, fname):
+    """Import file directly.
+
+    This is a hack to import files from packages without importing
+    <package>/__init__.py, its purpose is to allow import without requiring
+    all the dependencies at this point.
+
+    Args:
+        package (str): Package to import from
+        fname (str): File to import
+    Returns:
+        types.ModuleType: Imported module
+    """
     mod_name = fname.rstrip('.py')
     spec = spec_from_file_location(mod_name, '{}/{}'.format(package, fname))
     module = module_from_spec(spec)
     spec.loader.exec_module(module)
     return module
 
-# Hack to import _version file without importing bleeter/__init__.py, its
-# purpose is to allow import without requiring dependencies at this point.
-_version = import_file('bleeter', '_version.py')
+
+def make_list(s):
+    return s.strip().splitlines()
 
 
-with open('README.rst') as f:
-    long_description = f.read()
+def parse_requires(file):
+    deps = []
+    with open('extra/{}'.format(file)) as req_file:
+        entries = [s.split('#')[0].strip() for s in req_file.readlines()]
+    for dep in entries:
+        if not dep or dep.startswith('#'):
+            continue
+        elif dep.startswith('-r '):
+            deps.extend(parse_requires(dep.split()[1]))
+            continue
+        deps.append(dep)
+    return deps
 
-setup(
-    name='bleeter',
-    version=_version.dotted,
-    author='James Rowe',
-    author_email='jnrowe@gmail.com',
-    url='http://github.com/JNRowe/bleeter',
-    license='GPL-3',
-    description='Nasty little twitter client',
-    long_description=long_description,
-    data_files=[
-        ('share/pixmaps', ['bleeter.png', ]),
-        ('share/applications', ['bleeter.desktop']),
-    ],
-    packages=['bleeter', ],
-    entry_points={'console_scripts': ['bleeter = bleeter:main', ]},
-)
+
+conf = ConfigParser()
+conf.read('setup.cfg')
+metadata = dict(conf['metadata'])
+
+install_requires = parse_requires('requirements.txt')
+
+tests_require = parse_requires('requirements-test.txt')
+
+metadata = dict(conf['metadata'])
+for k in ['classifiers', 'packages', 'py_modules']:
+    if k in metadata:
+        metadata[k] = make_list(metadata[k])
+
+for k in ['include_package_data', ]:
+    if k in metadata:
+        metadata[k] = conf.getboolean('metadata', k)
+
+for k in ['data_files', 'entry_points', 'package_data']:
+    if k in metadata:
+        metadata[k] = eval(metadata[k], {'__builtins__': {}})
+
+with open('README.rst') as readme:
+    metadata['long_description'] = readme.read()
+
+_version = import_file(metadata['name'], '_version.py')
+
+if __name__ == '__main__':
+    setup(
+        version=_version.dotted,
+        install_requires=install_requires,
+        tests_require=tests_require,
+        cmdclass={'test': PytestTest},
+        zip_safe=False,
+        **metadata,
+    )
